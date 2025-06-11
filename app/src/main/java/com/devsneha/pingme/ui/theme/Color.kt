ce1063beb51ad134152e89c576a6da63eb39aa0e
sneha005 <bansalsneha991@gmail.com>
2025-06-11 18:20:31 +0530

    dark/light mode added

--- app/src/main/java/com/devsneha/pingme/ui/theme/Color.kt
+++ app/src/main/java/com/devsneha/pingme/ui/theme/Color.kt
@@ -2,10 +2,32 @@ package com.devsneha.pingme.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Light Theme Colors
+val PrimaryLight = Color(0xFF2196F3)
+val SecondaryLight = Color(0xFF03DAC6)
+val BackgroundLight = Color(0xFFF5F5F5)
+val SurfaceLight = Color(0xFFFFFFFF)
+val OnPrimaryLight = Color(0xFFFFFFFF)
+val OnSecondaryLight = Color(0xFF000000)
+val OnBackgroundLight = Color(0xFF1C1B1F)
+val OnSurfaceLight = Color(0xFF1C1B1F)
+val ErrorLight = Color(0xFFB00020)
+
+// Dark Theme Colors
+val PrimaryDark = Color(0xFF90CAF9)
+val SecondaryDark = Color(0xFF03DAC6)
+val BackgroundDark = Color(0xFF121212)
+val SurfaceDark = Color(0xFF1E1E1E)
+val OnPrimaryDark = Color(0xFF000000)
+val OnSecondaryDark = Color(0xFF000000)
+val OnBackgroundDark = Color(0xFFE1E1E1)
+val OnSurfaceDark = Color(0xFFE1E1E1)
+val ErrorDark = Color(0xFFCF6679)
+
+// Common Colors
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
-
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
